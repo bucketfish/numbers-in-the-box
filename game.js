004b0847dc3efe68a1fffbf1b335ba639747b64d
bucketfishy <bucketfishy@gmail.com>
2022-05-01 15:00:32 +0800

    shuffle array before arranging

--- game.js
+++ game.js
@@ -9,6 +9,23 @@ function intToChar(int) {
   return String.fromCharCode(code + int);
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 
 class Piece{
@@ -170,6 +187,7 @@ for (var i = 0; i < 5; i++){
 
 
 
+pieces = shuffle(pieces);
 
 for (var i = 0; i < count; i++){
   var piece = document.createElement("div");
